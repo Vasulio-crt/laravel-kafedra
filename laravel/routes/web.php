b15f567c82177884a047f1f08d467b028680a545
Vasulio-crt <vas_sab07@mail.ru>
2025-11-09 22:43:11 +0700

    Service Container, Service Providers и Facades

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,20 @@ Route::get('/', function () {
     dump(config('custom.custom_test', 'var not exist'));
     dump(config('custom.custom_test_env'));
     return view('welcome');
+});
+
+Route::get('/v3', function () {
+    // $cache = app()->make('cache');
+    // $cache->put('test', 123);
+    // dd($cache->get('test'));
+    
+    // $cache = app('cache');
+    
+    // $cache = cache();
+    // $cache->put('test', 2232);
+    // dd($cache->get('test'));
+
+    dump(Cache::get('test'));
+
+    dd(Illuminate\Support\Facades\App::make('cache'));
 });
