9d45f1af453b09b83c10cd0e7e4a4ddab6107b95
Vasulio-crt <vas_sab07@mail.ru>
2025-11-09 21:02:54 +0700

    Конфигурация Laravel

---
+++ laravel/config/custom.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'custom_test' => 'My var test',
+	'custom_test_env' => env('TEST_VAR', 'not exist')
+];
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // dump('hello');
+    // dump(config('database.connections.mysql'));
+    dump(config('custom.custom_test', 'var not exist'));
+    dump(config('custom.custom_test_env'));
     return view('welcome');
 });
