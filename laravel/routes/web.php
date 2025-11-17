69276853ffb214db8e6a3593bb3a62e84291ac23
Vasulio-crt <vas_sab07@mail.ru>
2025-11-17 20:16:33 +0700

    complite video4

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Router;
 
 Route::get('/', function () {
     // dump('hello');
@@ -26,3 +28,58 @@ Route::get('/v3', function () {
 
     dd(Illuminate\Support\Facades\App::make('cache'));
 });
+
+// ----Video 4----
+
+Route::get('/v4', function () {
+    return view('hi', ['title' => 'Main page']);
+});
+
+Route::view('/v4s', 'hi', ['title' => 'Main short page']);
+/* 
+Route::get('/posts/{id}', function ($id) {
+    return "Posts page, id: {$id}";
+});
+
+Route::get('/posts/{id?}', function ($id = 1) {
+    return "Posts page?, id: {$id}";
+});
+*/
+
+Route::get('/posts/{id}/comments/{comment_id}', function ($id, $comment_id) {
+    return "Posts page, id: {$id}. Comment id: {$comment_id}";
+});
+
+/*
+Route::get('/posts/{id}', function ($id) {
+    return "Posts page, id: {$id}";
+})->where(['id' => '[0-9]+']);
+// for global pattern app/Provides/AppServiceProvider.php
+*/
+
+Route::get('/posts/{id}', function ($id) {
+    return "Posts page, id: {$id}";
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    return "Posts SLUG, id: {$slug}";
+});
+
+Route::get('/search/{search}', function ($search) {
+    return "Search: {$search}";
+})->where(['search' => '.*']);
+
+Route::post('posts', function(){
+    return 'Store post';
+    // see bootstrap/app.php:15
+});
+
+Route::match(['get', 'post'], '/get-post', function() {
+    return 'Hello from GET|POST';
+})->withoutMiddleware(VerifyCsrfToken::class);
+
+Route::any('/anyMethods', function() {
+    return 'Hello from ANY';
+})->withoutMiddleware(VerifyCsrfToken::class);
+
+Route::redirect('/here', '/posts/777');
